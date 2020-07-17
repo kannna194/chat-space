4a30fc6487d210578a43d819e60bde285db1ab65
kannna194 <kannna194@gmail.com>
2020-07-17 16:01:52 +0900

    コントローラーの修正
    #What
    groups_controllerのupdateのif文修正。
    
    #Why
    コードエラーになるから

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,12 +19,11 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
-    
   end
 
   def update
     @group = Group.find(params[:id])
-    if current_groups.update(group_params)
+    if @group.update(group_params)
       redirect_to root_path, notice: 'グループを更新しました'
     else
         render :edit
