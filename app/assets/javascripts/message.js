68ac0e5dc8271d3797ab31bd0ad3cff9a39d77d3
kannna194 <kannna194@gmail.com>
2020-07-24 00:47:47 +0900

    HTMLを作成するメソッドを作成
    # What
    HTMLを作成するメソッドの作成
    非同期通信の結果をdone関数の引数で受け取り
    
    # Why
    ブラウザ上に表示するHTMLを用意するため。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,48 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+      `<div class="Message-posts">
+        <div class="Contributor">
+          <div class="Contributor__name">
+            ${message.user_name}
+          </div>
+          <div class="Contributor__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Message">
+          <div class="Message-comment">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else{
+      let html =
+      `<div class="Message-posts">
+        <div class="Contributor">
+          <div class="Contributor__name">
+            ${message.user_name}
+          </div>
+        <div class="Contributor__date">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="Message">
+        <div class="Message-comment">
+          <p class="Message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+
   $('.form').on('submit', function(e){
     e.preventDefault()
     let formData = new FormData(this)
@@ -11,5 +55,8 @@ $(function(){
       processData: false,
       contentType: false
     })
-  })
+    .done(function(data){
+      let html = buildHTML(data);
+    })
+  });
 });
